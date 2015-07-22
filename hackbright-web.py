f55ec4edfeeb2b487137b61cf92cec993915d209
Hackbright Student <info@hackbrightacademy.com>
2015-07-21 17:54:05 -0700

    added student search form

--- hackbright-web.py
+++ hackbright-web.py
@@ -10,7 +10,15 @@ def get_student():
 
     github = request.args.get('github', 'jhacks')
     first, last, github = hackbright.get_student_by_github(github)
-    return "%s is the GitHub account for %s %s" % (github, first, last)
+    html = render_template("student_info.html", github=github, first=first, last=last)
+
+    return html
+
+@app.route("/")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
